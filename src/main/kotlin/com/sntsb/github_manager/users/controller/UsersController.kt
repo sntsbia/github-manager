ffaa374ed0f3837677a812b9dac4e8aa0521f82c
sntsb <sntsb.doc@gmail.com>
2025-07-24 08:43:23 -0300

    feat: Implement user management features with role assignment and improved error handling

--- src/main/kotlin/com/sntsb/github_manager/users/controller/UsersController.kt
+++ src/main/kotlin/com/sntsb/github_manager/users/controller/UsersController.kt
@@ -1,13 +1,16 @@
 package com.sntsb.github_manager.users.controller
 
-import com.sntsb.github_manager.users.model.Users
+import com.sntsb.github_manager.users.model.dto.AssignRolesRequest
 import com.sntsb.github_manager.users.model.dto.CreateUsersRequest
 import com.sntsb.github_manager.users.service.UsersService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -16,17 +19,54 @@ class UsersController(
     private val usersService: UsersService
 ) {
     @PostMapping
-    fun createUser(@RequestBody userRequest: CreateUsersRequest): ResponseEntity<Users> {
+    @ResponseStatus(HttpStatus.CREATED)
+    fun createUser(@RequestBody userRequest: CreateUsersRequest): ResponseEntity<Any> {
+        println("Criando um novo usuário com o nome: ${userRequest.login}")
 
-        println("Criando um novo usuário com o nome: ${userRequest.name}")
-
-        val newUser = usersService.create(userRequest.name)
-
-        return newUser?.let {
-            ResponseEntity.status(HttpStatus.CREATED).body(newUser)
-        } ?: run {
-            ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null)
-        }
-
+        return usersService.create(userRequest.login).fold(
+            onSuccess = { newUser ->
+                ResponseEntity.status(HttpStatus.CREATED).body(newUser)
+            },
+            onFailure = { exception ->
+                val errorMessage = exception.message ?: "Ocorreu um erro desconhecido"
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).body(mapOf("error" to errorMessage))
+            }
+        )
     }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    fun getAll(): ResponseEntity<Any> {
+        println("Buscando todos os usuários")
+
+        return usersService.getAll().fold(
+            onSuccess = {usersList ->
+                ResponseEntity.status(HttpStatus.OK).body(usersList)
+            },
+            onFailure = {exception ->
+                val errorMessage = exception.message ?: "Ocorreu um erro desconhecido"
+
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).body(mapOf("error" to errorMessage))
+            }
+        )
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/{userId}/roles") // Defina o placeholder aqui
+    fun assignRole(@PathVariable("userId") userId: Long, @RequestBody request: AssignRolesRequest): ResponseEntity<Any> {
+
+        println("Adicionando a role ${request.rolesId} para o usuário${userId}")
+
+        return usersService.assignRoles(userId, request.rolesId).fold(
+            onSuccess = { newUser ->
+                ResponseEntity.status(HttpStatus.CREATED).body(newUser)
+            },
+            onFailure = { exception ->
+                val errorMessage = exception.message ?: "Ocorreu um erro desconhecido"
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).body(mapOf("error" to errorMessage))
+            }
+        )
+    }
+
+
 }
--- src/main/kotlin/com/sntsb/github_manager/users/repository/UsersRepository.kt
+++ src/main/kotlin/com/sntsb/github_manager/users/repository/UsersRepository.kt
@@ -2,5 +2,10 @@ package com.sntsb.github_manager.users.repository
 
 import com.sntsb.github_manager.users.model.Users
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.stereotype.Repository
 
-interface UsersRepository : JpaRepository<Users, Long>
+@Repository
+interface UsersRepository : JpaRepository<Users, Long> {
+
+}
