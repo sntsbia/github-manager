51c202c91fb3c0a286a842fb0818bb03ef1fc616
Beatriz Santos <52572077+sntsbia@users.noreply.github.com>
2025-07-25 12:20:23 -0300

    feat: Add swagger dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,7 @@ repositories {
 extra["springCloudVersion"] = "2025.0.0"
 
 dependencies {
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
